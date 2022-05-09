288202fad61d8b5f38ded98e5f9b9db858d070a6
Renan Roseno <renan.santos@mv.com.br>
2022-05-09 10:21:35 -0300

    Configure mongoDB to application

--- index.js
+++ index.js
@@ -1,4 +1,33 @@
-import cors from 'cors';
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
+import cors from "cors";
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import postRoutes from './routes/posts.js'
+
+const app = express();
+
+app.use('/post', postRoutes);
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL =
+  'mongodb+srv://rosenorenan:1SVG4p8516@social-media-cluster.piew5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log('server running on port ', PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
+
+
