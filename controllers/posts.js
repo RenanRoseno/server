17a56da9debfde757f4a589ce8a74332d771b940
Renan Roseno <rosenorenan@gmail.com>
2022-05-20 20:09:25 -0300

    Add edit functionality and fix bugs

--- controllers/posts.js
+++ controllers/posts.js
@@ -28,7 +28,7 @@ export const updatePost = async (req, res) => {
   if (!mongoose.Types.ObjectId.isValid(_id))
     return res.status(404).send("No post with that id");
 
-  const updatedPost = PostMessage.findByIdAndUpdate(_id, post, { new: true });
+  const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new: true });
 
   res.json(updatedPost);
 };
