a7882d77872a799113c55e3fd005ab0e5b7f6cba
Renan Roseno <rosenorenan@gmail.com>
2022-05-27 12:14:23 -0300

    Add middleware sheetn sigin and sign out endpoint

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import postRoutes from './routes/posts.js';
+import postRoutes from "./routes/posts.js";
+import userRoutes from "./routes/users.js";
 
 const app = express();
 dotenv.config();
@@ -12,8 +13,8 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.use('/post', postRoutes);
-
+app.use("/post", postRoutes);
+app.use("/user", userRoutes);
 // const CONNECTION_URL =
 //   'mongodb+srv://rosenorenan:1SVG4p8516@social-media-cluster.piew5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5001;
@@ -25,11 +26,9 @@ mongoose
   })
   .then(() => {
     app.listen(PORT, () => {
-      console.log('server running on port ', PORT);
+      console.log("server running on port ", PORT);
     });
   })
   .catch((error) => {
     console.log(error.message);
   });
-
-
